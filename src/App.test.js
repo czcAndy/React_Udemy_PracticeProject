4543c95585502f12a0651fc62b6e641515e075c5
Andrei Cazaceanu <andrei.cazaceanu@gmail.com>
2021-08-16 07:53:59 +0200

    Integration test added in App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe("Test App", () => {
   it('renders', () => {
     render(<App></App>)
   });
+
+  it('works Add User flow', () => {
+    const {getByText, getByLabelText} = render(<App/>);
+
+    fireEvent.change(getByLabelText(/Name/i), {target: {value: "User"}});
+    fireEvent.change(getByLabelText(/Age/i), {target: {value: "20"}});
+
+    const submitButton = getByText(/submit/i);
+    fireEvent.click(submitButton);
+
+    const user1Name = getByText(/^User$/i, {selector:'h3'});
+    const user1Age = getByText(/^20$/i, {selector: 'p'});
+
+    expect(user1Name).not.toBeNull();
+    expect(user1Age).not.toBeNull();
+  })
 })
