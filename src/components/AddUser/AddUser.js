7aa5f003e7dadbd9d2a65ee428968b65187936d2
Andrei Cazaceanu <andrei.cazaceanu@gmail.com>
2021-08-18 07:42:39 +0200

    Exchanges useState with useRef for user input handling

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,58 +1,51 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import ModalWindow from '../ModalWindow/ModalWindow.js'
 import styles from './AddUser.module.css';
 
 const AddUser = props => {
-    
-    var [name, setName] = useState('');
-    var [age, setAge] = useState(0);
-    var [errorMessage, setErrorMessage] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
 
+    var [errorMessage, setErrorMessage] = useState('');
     var[isInputValid, setInputIsvalid] = useState(true);
 
     const submitFormHandler = event => {
         event.preventDefault();
+        const enteredName = nameInputRef.current.value;
+        const enteredAge = ageInputRef.current.value;
 
-        if (name.length === 0 && age <= 0){
+        if (enteredName.length === 0 && enteredAge <= 0){
             setInputIsvalid(false);
             setErrorMessage("Make sure both the name and the age fields are filled in")
             return;
-        } else if (name.length === 0){
+        } else if (enteredName.length === 0){
             setInputIsvalid(false);
             setErrorMessage("Please enter your name")
             return;
-        } else if (age <= 0) {
+        } else if (enteredAge <= 0) {
             setErrorMessage("Please enter a valid age")
             setInputIsvalid(false);
             return;
         }
 
         var user = {
-            name: name,
-            age: age
+            name: enteredName,
+            age: enteredAge
         };
 
         props.onSubmit(user);
-    }
-
-    const inputNameChangeHandler = event => {
-        var name = event.target.value;
-        setName(name);
-    }
-
-    const inputAgeChangeHandler = event => {
-        var age = event.target.value;
-        setAge(age);
+        nameInputRef.current.value = ''
+        ageInputRef.current.value = ''
     }
 
     return (
         <>
          <form className={styles['form']} onSubmit={submitFormHandler}>
             <label className={styles['label-form']} htmlFor="name">Name</label>
-            <input className={styles['input-form']} id="name" type="text" name="name" onChange={inputNameChangeHandler}></input>
+            <input className={styles['input-form']} id="name" type="text" name="name" ref={nameInputRef}></input>
 
             <label className={styles['label-form']} htmlFor="age">Age</label>
-            <input className={styles['input-form']} id="age" type="number" name="age" min="1" onChange={inputAgeChangeHandler}></input>
+            <input className={styles['input-form']} id="age" type="number" name="age" min="1" ref={ageInputRef}></input>
 
             <input className={styles['primary-button']} type="submit" value="Submit"></input>
         </form>
